ee07116a273e7a61dfecbf1ad3222749946c1ca9
Moditha Marasingha <marasinghamoditha51@gmail.com>
2024-08-06 17:17:19 +0530

    Main Template of Website Created

---
+++ contact.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ADD PAGE TITLE</title>
+    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="css/partials.css">
+</head>
+<body>
+    <?php include ("partials/header.php"); ?>
+
+
+    
+    <?php include ("partials/footer.php"); ?>
+    <script src="js/partials.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ADD PAGE TITLE</title>
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/partials.css">
 </head>
 <body>
+    <?php include ("partials/header.php"); ?>
+
+
     
+    <?php include ("partials/footer.php"); ?>
+    <script src="js/index.js"></script>
+    <script src="js/partials.js"></script>
 </body>
 </html>
-
-<?php 
-    
-?>
