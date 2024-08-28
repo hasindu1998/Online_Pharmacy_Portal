bb43b7c9528a5061c563e102831a8833613522a3
Hasindu Chanuka <chanukasankalpa456@gmail.com>
2024-08-28 15:04:00 +0530

    Footer Created

--- footer.php
+++ footer.php
@@ -10,7 +10,44 @@
 </head>
 <body>
     <footer>
-        
+        <div class="footer-container">
+            <div class="container">
+                <div class="footer-content">
+                    <img src="./Images/Pharmacy X.png" alt="" width="150px">
+    
+                    <p class="footer-details">166/2, Havelock Road, Colombo 05</p>
+
+                    <div class="social-media-icons-container">
+                        
+                    </div>
+                </div>
+    
+                <div class="footer-content">
+                    <h3 class="footer-heading">Products</h3>
+                    <p class="footer-details">&#10148 Vitamins</p>
+                    <p class="footer-details">&#10148 Skin Care</p>
+                    <p class="footer-details">&#10148
+                        Pain Killers</p>
+                </div>
+                
+    
+                <div class="footer-content">
+                    <h3 footer-heading>Support</h3>
+                    <p class="footer-details">&#10148 Pricing</p>
+                    <p class="footer-details">&#10148
+                        Documentaion</p>
+                    <p class="footer-details">&#10148
+                        Guides</p>
+                </div>
+    
+                <div class="footer-content">
+                    <h3 footer-heading>Company</h3>
+                    <p class="footer-details">&#10148 <a href="" class="footer-links">About</a></p>
+                    <p class="footer-details">&#10148 <a href="" class="footer-links">FAQ</a></p>
+                    <p class="footer-details">&#10148 <a href="" class="footer-links">Contact</a></p>
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
 </html>
