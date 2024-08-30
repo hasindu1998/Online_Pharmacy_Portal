62d9521e6118301a6499ad24f8efa461c8bd48e5
Moditha Marasingha <marasinghamoditha51@gmail.com>
2024-08-30 10:41:08 +0530

    My Orders Page Done

--- my_orders.php
+++ my_orders.php
@@ -15,7 +15,64 @@
 <body>
     <?php include ("./header.php"); ?>
 
-    
+    <div class="container1">
+        <h2>My Orders</h2>
+
+        <div class="table_container">
+            <table>
+                <tr>
+                    <th>Order ID</th>
+                    <th>Order Date</th>
+                    <th>Product</th>
+                    <th>Order Status</th>
+                    <th>Order Total</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>2021-08-01</td>
+                    <td>Panadol</td>
+                    <td><span class="status">Delivered</span></td>
+                    <td>Rs. 5000</td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>2021-08-05</td>
+                    <td>Siddhalepa Balm</td>
+                    <td><span class="status">Delivered</span></td>
+                    <td>Rs. 3000</td>
+                </tr>
+                <tr>
+                    <td>3</td>
+                    <td>2021-08-10</td>
+                    <td>Whitening Cream</td>
+                    <td><span class="status">Delivered</span></td>
+                    <td>Rs. 7000</td>
+                </tr>
+                <tr>
+                    <td>4</td>
+                    <td>2021-08-15</td>
+                    <td>Mass Gainer</td>
+                    <td><span class="status">Processing</span></td>
+                    <td>Rs. 9000</td>
+                </tr>
+                <tr>
+                    <td>5</td>
+                    <td>2021-08-20</td>
+                    <td>Vitamin C</td>
+                    <td><span class="status">Pending Approval</span></td>
+                    <td>Rs. 12000</td>
+                </tr>
+                <tr>
+                    <td>6</td>
+                    <td>2021-08-25</td>
+                    <td>Vitamin E</td>
+                    <td><span class="status">Pending Approval</span></td>
+                    <td>Rs. 2000</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+
 
     <?php include ("./footer.php"); ?>
 </body>
