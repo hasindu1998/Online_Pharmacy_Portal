0ae91722ff296ad1617221a45863cda3b77f9c10
Moditha Marasingha <marasinghamoditha51@gmail.com>
2024-08-30 09:22:51 +0530

    created base template

--- aboutUs.php
+++ aboutUs.php
@@ -4,13 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us</title>
-    <link rel="stylesheet" href="./CSS/about.css">
+    <link rel="stylesheet" href="./CSS/index.css">
+    <link rel="stylesheet" href="./CSS/aboutUs.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="./Images/Pharmacy X Icon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./Images/Pharmacy X Icon.png">
 </head>
 <body>
     <?php include ("./header.php"); ?>
 
-
     
+
     <?php include ("./footer.php"); ?>
 </body>
 </html>
---
+++ addDelete_Product.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manage Products</title>
+    <link rel="stylesheet" href="./CSS/index.css">
+    <link rel="stylesheet" href="./CSS/addDelete_Product.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="./Images/Pharmacy X Icon.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./Images/Pharmacy X Icon.png">
+</head>
+<body>
+    <?php include ("./header.php"); ?>
+
+    
+
+    <?php include ("./footer.php"); ?>
+</body>
+</html>
