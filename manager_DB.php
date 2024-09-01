b175d071965a77485aacdf5cfb72f037b4cb9e74
Hasindu Chanuka <chanukasankalpa456@gmail.com>
2024-09-02 00:38:13 +0530

    Product Page Created

--- manager_DB.php
+++ manager_DB.php
@@ -17,71 +17,69 @@
 <body>
   <?php include("./header.php"); ?>
 
-  <div class="orders-container">
-    <h1 class="manager-headings">Orders</h1>
-    <div class="order-table">
+  <div class="manager-dashboard-container">
+    <div class="orders-container">
+      <h1 class="manager-headings">Orders</h1>
+      <div class="order-table">
 
-    </div>
-  </div>
-
-  <div class="total-amount-container">
-    <h2 class="total-amount-heading">Today Earned Amount: </h2>
-    <div class="total-amount-show-box"></div>
-  </div>
-
-  <div class="manage-admin-container">
-    <div class="add-admin-container">
-      <h1 class="manager-headings">Add Admin</h1>
-
-      <div class="admin-form-container">
-        <form action="manager_DB.php" method="POST">
-          <div class="admin-form">
-            <div class="form-rows">
-              <input type="text" class="form-input-box" placeholder="First Name">
-              <input type="text" class="form-input-box-right" placeholder="Last Name">
-            </div>
-            <div class="form-rows">
-              <input type="text" class="form-input-box" placeholder="Username">
-              <input type="text" class="form-input-box-right" placeholder="E mail">
-            </div>
-            <div class="form-rows">
-              <input type="text" class="form-input-box" placeholder="Enter Password">
-              <input type="text" class="form-input-box-right" placeholder="Re-Enter Password">
-            </div>
-            <div class="add-admin-button-container">
-              <button type="submit" class="add-admin-button">Add Admin</button>
-            </div>
-          </div>
-        </form>
       </div>
     </div>
 
-    <div class="control-admins">
-      <h1 class="manager-headings">Manage Admins</h1>
+    <div class="total-amount-container">
+      <h2 class="total-amount-heading">Today Earned Amount: </h2>
+      <div class="total-amount-show-box"></div>
+    </div>
 
-      <div class="control-admin-form">
-        <div class="check-admin">
+    <div class="manage-admin-container">
+      <div class="add-admin-container">
+        <h1 class="manager-headings">Add Admin</h1>
+
+        <div class="admin-form-container">
           <form action="manager_DB.php" method="POST">
-            <input type="text" class="check-admin-input" placeholder="Username">
-            <button type="submit" class="check-admin-button">Check</button>
+            <div class="admin-form">
+              <div class="form-rows">
+                <input type="text" class="form-input-box" placeholder="First Name">
+                <input type="text" class="form-input-box-right" placeholder="Last Name">
+              </div>
+              <div class="form-rows">
+                <input type="text" class="form-input-box" placeholder="Username">
+                <input type="text" class="form-input-box-right" placeholder="E mail">
+              </div>
+              <div class="form-rows">
+                <input type="text" class="form-input-box" placeholder="Enter Password">
+                <input type="text" class="form-input-box-right" placeholder="Re-Enter Password">
+              </div>
+              <div class="add-admin-button-container">
+                <button type="submit" class="add-admin-button">Add Admin</button>
+              </div>
+            </div>
           </form>
         </div>
-
-        <div class="admin-satus-container">
-          <h3 class="status-heading">Status : </h3>
-          <div class="admin-status">Active</div>
-        </div>
-
-        <div class="admin-control-buttons">
-          <button class="activate">Activate</button>
-          <button class="deactivate">Deactivate</button>
-          <button class="delete">Delete</button>
-        </div>
-
-
       </div>
 
+      <div class="control-admins">
+        <h1 class="manager-headings">Manage Admins</h1>
 
+        <div class="control-admin-form">
+          <div class="check-admin">
+            <form action="manager_DB.php" method="POST">
+              <input type="text" class="check-admin-input" placeholder="Username">
+              <button type="submit" class="check-admin-button">Check</button>
+            </form>
+          </div>
+
+          <div class="admin-satus-container">
+            <h3 class="status-heading">Status : </h3>
+            <div class="admin-status">Active</div>
+          </div>
+
+          <div class="admin-control-buttons">
+            <button class="activate">Activate</button>
+            <button class="deactivate">Deactivate</button>
+            <button class="delete">Delete</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
--- products.php
+++ products.php
@@ -14,8 +14,226 @@
 </head>
 <body>
     <?php include ("./header.php"); ?>
-
+    <div class="main-image-container">
+        <div class="main-heading-container">
+            <h1 class="main-heading">Order Medicine Easily by Uploading Prescription</h1>
+            <button class="main-heading-button">Try Now</button>
+        </div>
+        <img src="./Images\Product-Page-Image.jpg" alt="" class="main-image">
+        </div>
+    </div>
     
+    <div class="products-section-container">
+        <div class="products-heading-container">
+            <div class="product-heading">Products</div>
+        </div>
+
+        <div class="products-showing-container">
+            <div class="product-content-container">
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-1.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-2.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-3.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-4.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-5.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-9.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-content-container">
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-6.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-7.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-8.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-9.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-1.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-9.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="product-content-container">
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-2.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-3.png" alt="" class="item-image">
+                    </div>
+                    <div class="iproduct-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-4.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-5.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-6.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+
+                <div class="products-container">
+                    <div class="product-image-container">
+                        <img src="./Images\product-icons\Pharmacy-Isometric-Icons-9.png" alt="" class="item-image">
+                    </div>
+                    <div class="product-details-container">
+                        <p class="item-name">Panadol</p>
+                        <p class="item-price">Rs.100.00</p>
+                        <button class="item-buynow-button">Buy Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <?php include ("./footer.php"); ?>
 </body>
