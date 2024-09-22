5032b6d81f66b0e1bc0a6a0c7aaca2b55f7d42f6
PabodaWA <wampaboda@gmail.com>
2024-09-22 18:23:36 +0530

    register page

--- register.php
+++ register.php
@@ -8,13 +8,62 @@
     <link rel="stylesheet" href="./CSS/register.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" href="register.css>
     <link rel="icon" type="image/png" sizes="32x32" href="./Images/Pharmacy X Icon.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./Images/Pharmacy X Icon.png">
 </head>
 <body>
     
-
+<div class="container">
+        <h2>Registration</h2>
+        <form id="registrationForm">
+            <div class="form-row">
+                <div class="form-column">
+                    <div class="form-group">
+                        <label for="fullName">Full Name</label>
+                        <input type="text" id="fullName" placeholder="Enter Your Full Name" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" placeholder="mail@example.com" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" placeholder="Minimum of 8 characters" required>
+                    </div>
+                </div>
+                
+                <div class="form-column">
+                    <div class="form-group">
+                        <label for="username">User Name</label>
+                        <input type="text" id="username" placeholder="Enter Your User Name" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="phone">Phone Number</label>
+                        <input type="tel" id="phone" placeholder="Enter Your Phone Number" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="confirmPassword">Confirm Password</label>
+                        <input type="password" id="confirmPassword" placeholder="Minimum of 8 characters" required>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="form-group gender">
+                <label>Gender</label>
+                <input type="radio" name="gender" value="male" id="male" required> Male
+                <input type="radio" name="gender" value="female" id="female"> Female
+                <input type="radio" name="gender" value="none" id="none"> Prefer not to say
+            </div>
+            
+            <button type="submit">Register</button>
+        </form>
+    </div>
+    <script src="/js/order.js"></script>
     
 
     
