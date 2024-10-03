c4e7ba1932e0d1e3b555e07cb7ba8b41990e03f6
Moditha Marasingha <marasinghamoditha51@gmail.com>
2024-10-04 00:04:28 +0530

    Update header.php

--- header.php
+++ header.php
@@ -17,8 +17,8 @@
             <nav>
                 <ul>
                     <li><a href="index.php">Home</a></li>
-                    <li><a href="aboutUs.php">About</a></li>
                     <li><a href="products.php">Products</a></li>
+                    <li><a href="aboutUs.php">About</a></li>
                     <li><a href="contact.php">Contact</a></li>
                 </ul>
             </nav>
